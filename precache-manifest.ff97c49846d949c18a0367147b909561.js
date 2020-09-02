9e4b0204ea34af2966c6645d70c043040b20598d
unknown <andres_perlo@gmail.com>
2020-09-02 19:20:36 -0300

    Updates

---
+++ precache-manifest.ff97c49846d949c18a0367147b909561.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a3361c8dbf1ade7576931eb196e1fc4",
+    "url": "/demo-enriqueserra/index.html"
+  },
+  {
+    "revision": "48f444598ae4ab200ab0",
+    "url": "/demo-enriqueserra/static/css/main.f84651e7.chunk.css"
+  },
+  {
+    "revision": "357e942a87923b1ec9b5",
+    "url": "/demo-enriqueserra/static/js/2.52189def.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/demo-enriqueserra/static/js/2.52189def.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48f444598ae4ab200ab0",
+    "url": "/demo-enriqueserra/static/js/main.b72a32ab.chunk.js"
+  },
+  {
+    "revision": "427158f177eb01f3e84b",
+    "url": "/demo-enriqueserra/static/js/runtime-main.852ad51d.js"
+  },
+  {
+    "revision": "036615077f831dae179a90e231a02b65",
+    "url": "/demo-enriqueserra/static/media/PortadaA.03661507.jfif"
+  },
+  {
+    "revision": "5d063b70d24e6065908a1d83a9c1d132",
+    "url": "/demo-enriqueserra/static/media/PortadaOro.5d063b70.jfif"
+  },
+  {
+    "revision": "882fc3de235770293c2720b049f01070",
+    "url": "/demo-enriqueserra/static/media/PortadaPlata.882fc3de.jfif"
+  },
+  {
+    "revision": "6a879d011dc31b9f04ff6cbae89188b7",
+    "url": "/demo-enriqueserra/static/media/presentacion.6a879d01.jfif"
+  },
+  {
+    "revision": "a2a6574173a15512ce611a46da9aab87",
+    "url": "/demo-enriqueserra/static/media/presentacion2.a2a65741.jfif"
+  },
+  {
+    "revision": "f2b939df08c58640eeb4b45a793bd553",
+    "url": "/demo-enriqueserra/static/media/presentacion3.f2b939df.jfif"
+  }
+]);
