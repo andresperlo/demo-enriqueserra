ed75d677f740ae53f704e8bceb2a0112a6bd5fb6
unknown <andres_perlo@gmail.com>
2020-09-03 23:54:49 -0300

    se ajustan mapas

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -6,8 +6,8 @@ function ContactPage() {
             <div className='container-fluid'>
                 <div className='d-flex justify-content-center'>
                     <div className='ConcepcionOfi'>
-                        <h6 className='text-center py-3'>Oficina Central en Concepcion</h6>
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d627.5852330427691!2d-65.59458889355633!3d-27.344125078126858!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9423cf682081a851%3A0x9258390fc1683301!2sEmpresa%20Enrique%20Serra!5e0!3m2!1ses!2sar!4v1599145559742!5m2!1ses!2sar" frameborder="0" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                        <h4 className='text-center py-3'>Oficina Central en Concepcion</h4>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d627.5852330427691!2d-65.59458889355633!3d-27.344125078126858!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9423cf682081a851%3A0x9258390fc1683301!2sEmpresa%20Enrique%20Serra!5e0!3m2!1ses!2sar!4v1599145559742!5m2!1ses!2sar" frameborder="0" allowfullscreen="" aria-hidden="false" tabindex="0" className='border border-dark'></iframe>
                         <div className='OficinaConcepcionCss pt-4'>
                             <div className='d-flex'>
                                 <i className="fas fa-map-marker-alt"></i>
@@ -26,8 +26,9 @@ function ContactPage() {
                             <div className='d-flex'>
                                 <i className="fas fa-phone-volume"></i>
                                 <div className='ml-3'>
-                                    <h6>Telefono</h6>
-                                    <h5>3865 000 000</h5>
+                                    <h6>Telefonos</h6>
+                                    <h5>03865 422 684</h5>
+                                    <h5>03865 422 273</h5>
                                 </div>
                             </div>
                         </div>
@@ -36,7 +37,7 @@ function ContactPage() {
                 <div className='py-5'>
                     <h3>Sucursales</h3>
                 </div>
-                <div className='d-flex justify-content-center SucursalesCss'>
+                <div className='SucursalesCss'>
                     <div>
                         <h5 className='text-center'>San Miguel de Tucuman</h5>
                         <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d445.04876245861215!2d-65.21128881216431!3d-26.827542057630364!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xc899034ddc3540fb!2sEmpresa%20Enrique%20Serra!5e0!3m2!1ses!2sar!4v1599164531175!5m2!1ses!2sar" frameborder="0" allowfullscreen="" aria-hidden="false" tabindex="0" className='border border-dark'></iframe>
@@ -46,43 +47,44 @@ function ContactPage() {
                         </div>
                         <div className='d-flex ml-3'>
                             <i className="fas fa-phone-volume"></i>
-                            <h6 className='ml-2'>381 000 000</h6>
+                            <h6 className='ml-2'>0381 431 0756 / </h6>
+                            <h6 className='ml-2'>0381 431 0947</h6>
                         </div>
                     </div>
-                    <div className='mx-4'>
+                    <div className='SucursalAlberdicss'>
                         <h5 className='text-center'>J.B Alberdi</h5>
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d445.04876245861215!2d-65.21128881216431!3d-26.827542057630364!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xc899034ddc3540fb!2sEmpresa%20Enrique%20Serra!5e0!3m2!1ses!2sar!4v1599164531175!5m2!1ses!2sar" frameborder="0" allowfullscreen="" aria-hidden="false" tabindex="0" className='border border-dark'></iframe>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d442.62826337521903!2d-65.61623760660041!3d-27.588479156213637!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9423e902451d8f61%3A0x763541850a3dd87d!2sEmpresa%20Enrique%20Serra!5e0!3m2!1ses!2sar!4v1599187277681!5m2!1ses!2sar" frameborder="0" allowfullscreen="" aria-hidden="false" tabindex="0" className='border border-dark'></iframe>
                         <div className='d-flex ml-3'>
                             <i className="fas fa-map-marker-alt"></i>
                             <h6 className='ml-2'>San Martin 999</h6>
                         </div>
                         <div className='d-flex ml-3'>
                             <i className="fas fa-phone-volume"></i>
-                            <h6 className='ml-2'>381 000 000</h6>
+                            <h6 className='ml-2'>03865 471 099</h6>
                         </div>
                     </div>
                     <div>
                         <h5 className='text-center'>Aguilares</h5>
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d445.04876245861215!2d-65.21128881216431!3d-26.827542057630364!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xc899034ddc3540fb!2sEmpresa%20Enrique%20Serra!5e0!3m2!1ses!2sar!4v1599164531175!5m2!1ses!2sar" frameborder="0" allowfullscreen="" aria-hidden="false" tabindex="0" className='border border-dark'></iframe>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d566.9524303196043!2d-65.61491570293968!3d-27.42932278470274!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9423c5ab69b9beb3%3A0x258069764b8fdc30!2sEmpresa%20Serra!5e0!3m2!1ses!2sar!4v1599187482376!5m2!1ses!2sar" frameborder="0" allowfullscreen="" aria-hidden="false" tabindex="0" className='border border-dark'></iframe>
                         <div className='d-flex ml-3'>
                             <i className="fas fa-map-marker-alt"></i>
-                            <h6 className='ml-2'>San Martin 999</h6>
+                            <h6 className='ml-2'>C. Pellegrini 642</h6>
                         </div>
                         <div className='d-flex ml-3'>
                             <i className="fas fa-phone-volume"></i>
-                            <h6 className='ml-2'>381 000 000</h6>
+                            <h6 className='ml-2'>03865 481 129</h6>
                         </div>
                     </div>
-                    <div className='ml-4'>
+                    <div className='SucursalLaCochacss'>
                         <h5 className='text-center'>La Cocha</h5>
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d445.04876245861215!2d-65.21128881216431!3d-26.827542057630364!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xc899034ddc3540fb!2sEmpresa%20Enrique%20Serra!5e0!3m2!1ses!2sar!4v1599164531175!5m2!1ses!2sar" frameborder="0" allowfullscreen="" aria-hidden="false" tabindex="0" className='border border-dark'></iframe>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d578.683953978351!2d-65.58697312162099!3d-27.764541952552154!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9423f16e5ce4a60d%3A0xb7ec71d31db2d789!2sSan%20Mart%C3%ADn%20Norte%2C%20La%20Cocha%2C%20Tucum%C3%A1n!5e0!3m2!1ses!2sar!4v1599187683390!5m2!1ses!2sar"  frameborder="0"  allowfullscreen="" aria-hidden="false" tabindex="0" className='border border-dark'></iframe>
                         <div className='d-flex ml-3'>
                             <i className="fas fa-map-marker-alt"></i>
-                            <h6 className='ml-2'>San Martin 999</h6>
+                            <h6 className='ml-2'>San Martin 61</h6>
                         </div>
                         <div className='d-flex ml-3'>
                             <i className="fas fa-phone-volume"></i>
-                            <h6 className='ml-2'>381 000 000</h6>
+                            <h6 className='ml-2'>03865 496 124</h6>
                         </div>
                     </div>
                 </div>
